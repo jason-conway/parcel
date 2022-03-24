5547a7e41ce21d6fd87eaba7984d0e08ab082746
Jason Conway <jpc@jasonconway.dev>
2022-03-23 21:44:25 -0500

    Bump version

--- src/key-exchange.h
+++ src/key-exchange.h
@@ -2,7 +2,7 @@
  * @file key-exchange.c
  * @author Jason Conway (jpc@jasonconway.dev)
  * @brief Parcel Client-Server Key Exchange
- * @version 0.1
+ * @version 0.9.1
  * @date 2022-02-01
  *
  * @copyright Copyright (c) 2022 Jason Conway. All rights reserved.
@@ -19,5 +19,4 @@ void print_fingerprint(const char *str, const uint8_t *fingerprint);
 int two_party_client(const sock_t socket, uint8_t *key, uint8_t *fingerprint);
 int two_party_server(const sock_t socket, uint8_t *session_key);
 int node_key_exchange(const sock_t socket, size_t rounds, uint8_t *session_key, uint8_t *fingerprint);
-// int key_exchange_router(sock_t server, fd_set *connections, size_t max_in_set, uint8_t *key);
 int key_exchange_router(sock_t *sockets, size_t connections, uint8_t *key);
