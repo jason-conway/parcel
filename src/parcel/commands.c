2f5188f330aa5323881b6adf8e79e7dba920d02b
Jason Conway <jpc@jasonconway.dev>
2022-03-25 18:16:25 -0500

    Revised username prepend function, add length checking on new usernames

--- src/parcel/commands.c
+++ src/parcel/commands.c
@@ -2,7 +2,7 @@
  * @file commands.c
  * @author Jason Conway (jpc@jasonconway.dev)
  * @brief parcel client commands
- * @version 0.1
+ * @version 0.9.1
  * @date 2022-03-14
  * 
  * @copyright Copyright (c) 2022 Jason Conway. All rights reserved.
@@ -12,49 +12,52 @@
 #include "client.h"
 
 // Prepend client username to message string
-static void prepend_username(client_t *ctx, char **message, size_t *message_length)
+static void prepend_username(char *username, char **plaintext, size_t *plaintext_length)
 {
-	*message_length += strlen(ctx->username) + 2; // +2 for ": "
-
-	char *msg = malloc(*message_length);
-	if (!msg) {
-		free(*message);
-		*message = NULL;
-		return;
+	static const char *seperator = ": ";
+	bool error = true;
+	char *named_message = xstrcat(3, username, seperator, *plaintext);
+	if (named_message) {
+		*plaintext_length = strlen(named_message);
+		error = false;
 	}
-
-	if (snprintf(msg, *message_length, "%s: %s", ctx->username, *message) != (int)*message_length) {
-		free(*message);
-		*message = NULL;
-		return;
-	}
-
-	free(*message);
-	*message = msg;
+	free(*plaintext);
+	*plaintext = error ? NULL : named_message;
 }
 
 static int cmd_username(client_t *ctx, char **message, size_t *message_length)
 {
 	free(*message);
-	char *new_username = NULL;
-	size_t new_username_length = 0;
-	do {
-		printf("> New username: ");
-		fflush(stdout);
-		xgetline(&new_username, &new_username_length, stdin);
-	} while (!new_username_length);
-	new_username[new_username_length - 1] = '\0'; // remove \n
+	
+	char *new_username;
+	size_t new_username_length;
+	
+	while (1) {
+		new_username = NULL;
+		new_username_length = 0;
+
+		do {
+			printf("> New username: ");
+			fflush(stdout);
+			xgetline(&new_username, &new_username_length, stdin);
+		} while (!new_username_length);
+
+		if (new_username_length > USERNAME_MAX_LENGTH) {
+			printf("> Maximum username length is %d characters\n", USERNAME_MAX_LENGTH);
+			free(new_username);
+			continue;
+		}
+		break;
+	}
 
 	static const char *template = "\033[33m%s has changed their username to %s\033[0m";
 	const size_t msg_length = strlen(template) + new_username_length + strlen(ctx->username) + 1;
 	char *msg = malloc(msg_length);
 	if (!msg) {
-		send_connection_status(ctx, true);
-		return -1;
+		goto error;
 	}
 	if (snprintf(msg, msg_length, template, ctx->username, new_username) < 0) {
-		send_connection_status(ctx, true);
-		return -1;
+		goto error;
 	}
 
 	*message = msg;
@@ -63,14 +66,16 @@ static int cmd_username(client_t *ctx, char **message, size_t *message_length)
 	memcpy(ctx->username, new_username, new_username_length);
 	free(new_username);
 	return 0;
+
+	error:
+		free(new_username);
+		send_connection_status(ctx, true);
+		return -1;
 }
 
 // TODO
 static int cmd_send_file(client_t *ctx, char **message, size_t *message_length)
 {
-	(void)ctx;
-	(void)message_length;
-	
 	char *file_path = NULL;
 	size_t path_length = 0;
 	do {
@@ -78,38 +83,34 @@ static int cmd_send_file(client_t *ctx, char **message, size_t *message_length)
 		fflush(stdout);
 		xgetline(&file_path, &path_length, stdin);
 	} while (!path_length);
-	file_path[path_length - 1] = '\0'; // remove \n
 	
-	// Check if file is valid
-	// Open file
-	// Read file into buffer at *message
-	// Update message_length
-
 	if (!xfexists(file_path)) {
 		xprintf(YEL, "File \"%s\" not found\n", file_path);
-		return -1;
+		goto error_path_only;
 	}
 
 	size_t file_size = xfsize(file_path); 
 	if (!file_size) {
 		xprintf(YEL, "Unable to determine size of file \"%s\"\n", file_path);
-		return -1;
+		goto error_path_only;
 	}
+	
 	// TODO: make enum
 	const size_t max_file = DATA_LEN_MAX - BLOCK_LEN;
 	if (file_size > max_file) {
 		xprintf(YEL, "File \"%s\" is %zu bytes over the maximum supported size of %d bytes\n", file_path, (size_t)(file_size - max_file), max_file);
-		return -1;
+		goto error_path_only;
 	}
 
-	uint8_t *file_contents = malloc(file_size + BLOCK_LEN);
+	*message_length = file_size + BLOCK_LEN;
+	uint8_t *file_contents = malloc(*message_length);
 	if (!file_contents) {
-		return -1;
+		goto error;
 	}
 	FILE *file = fopen(file_path, "rb");
 	if (!file) {
 		xprintf(RED, "Could not open file \"%s\" for reading\n", file_path);
-		return -1;
+		goto error;
 	}
 	
 	// First block will be the file name
@@ -117,20 +118,34 @@ static int cmd_send_file(client_t *ctx, char **message, size_t *message_length)
 
 	if (fread(&file_contents[16], 1, file_size, file) != file_size) {
 		xprintf(RED, "Error reading contents of file \"%s\"\n", file_path);
-		return -1;
+		(void)fclose(file);
+		goto error;
 	}
+
 	(void)fclose(file);
+
+	free(file_path);
 	free(*message);
+
 	*message = (char *)file_contents;
+
+	return 0;
+
+	error:
+		free(file_contents);
+	error_path_only:
+		free(file_path);
+		send_connection_status(ctx, true);
+		return -1;
+}
+
+static inline int cmd_print_fingerprint(const uint8_t *fingerprint)
+{
+	print_fingerprint("Fingerprint is: ", fingerprint);
 	return 0;
 }
 
-static inline void cmd_print_fingerprint(const uint8_t *fingerprint)
-{
-	print_fingerprint("Fingerprint is: ", fingerprint);
-}
-
-_Noreturn static void cmd_exit(client_t *ctx, char *message)
+noreturn void cmd_exit(client_t *ctx, char *message)
 {
 	free(message);
 	send_connection_status(ctx, true);
@@ -157,24 +172,22 @@ static enum command_id parse_command(char *command)
 int parse_input(client_t *ctx, char **message, size_t *message_length)
 {
 	if (*message[0] != ':') { // Fast return
-		prepend_username(ctx, message, message_length);
-		if (!(*message)) {
+		prepend_username(ctx->username, message, message_length);
+		if (!*message) {
 			return -1;
 		}
-		return CMD_NONE;
+		return SEND_TEXT;
 	}
 	else {
 		switch (parse_command(*message)) {
 			case CMD_EXIT:
 				cmd_exit(ctx, *message);
 			case CMD_USERNAME:
-				return cmd_username(ctx, message, message_length) ? -1 : CMD_USERNAME;
+				return cmd_username(ctx, message, message_length) ? -1 : SEND_TEXT;
 			case CMD_FINGERPRINT:
-				cmd_print_fingerprint(ctx->fingerprint);
-				return CMD_FINGERPRINT;
+				return cmd_print_fingerprint(ctx->fingerprint) ? -1 : SEND_NONE;
 			case CMD_FILE:
-				cmd_send_file(ctx, message, message_length);
-				return CMD_FILE;
+				return cmd_send_file(ctx, message, message_length) ? -1 : SEND_FILE;
 			default:
 				return -1;
 		}
