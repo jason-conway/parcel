d7f066d6d1418729c67055beb763d59882ac467e
Jason Conway <jpc@jasonconway.dev>
2022-04-20 20:21:48 -0500

    Fix todos output

--- todos.sh
+++ todos.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-printf "parcel binary size:      %s  (%s)\n" "$(du -hs ./build/parcel)"  "$(cat ./build/parcel | wc -c)"
+printf "parcel binary size:      %s   (%s)\n" "$(du -hs ./build/parcel)"  "$(cat ./build/parcel | wc -c)"
 printf "parceld binary size:     %s  (%s)\n" "$(du -hs ./build/parceld)" "$(cat ./build/parceld | wc -c)"
-printf "sloc:    %s\n" "$(cat src/*.* src/parcel/* src/parceld/* | egrep -v '^$' | egrep -v '^\w*\/\/' | wc -l)"
-printf "TODOs:         %d\n\n" "$(grep -rHni todo src/ | wc -l)"
-bash -c "git grep -n TODO"
-bash -c "grep -rHni todo src/ "
+printf "source lines of code:    %d\n" "$(cat src/*.* src/parcel/* src/parceld/* | egrep -v '^$' | egrep -v '^\w*\/\/' | wc -l)"
+printf "number of TODOs:         %d\n" "$(grep -rHni todo src/ | wc -l)"
+bash -c "grep -rHni todo src"
