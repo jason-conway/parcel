03bb9c66241cee82855cc20a0b4be1f1a8e46fc2
Jason Conway <jpc@jasonconway.dev>
2022-04-26 22:43:18 -0500

    Add Windows support

--- todos.sh
+++ todos.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+EXE=""
+OS="$(uname)"
+if [[ $OS == "Windows_NT" ]]; then
+	EXE='.exe'
+fi
 
-printf "parcel binary size:      %s   (%s)\n" "$(du -hs ./build/parcel)"  "$(cat ./build/parcel | wc -c)"
-printf "parceld binary size:     %s  (%s)\n" "$(du -hs ./build/parceld)" "$(cat ./build/parceld | wc -c)"
+printf "parcel binary size:      %s   (%s)\n" "$(du -hs ./build/parcel${EXE})" "$(cat ./build/parcel${EXE} | wc -c)"
+printf "parceld binary size:     %s  (%s)\n" "$(du -hs ./build/parceld${EXE})" "$(cat ./build/parceld${EXE} | wc -c)"
 printf "source lines of code:    %d\n" "$(cat src/*.* src/parcel/* src/parceld/* | egrep -v '^$' | egrep -v '^\w*\/\/' | wc -l)"
-printf "number of TODOs:         %d\n" "$(grep -rHni todo src/ | wc -l)"
+printf "number of todos:         %d\n" "$(grep -rHni todo src/ | wc -l)"
 bash -c "grep -rHni todo src"
